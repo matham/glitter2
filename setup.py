4a89e09d898b2837c789d753e45c52203f45011a
Matthew Einhorn <moiein2000@gmail.com>
2020-10-08 18:51:39 -0400

    Pin nixio==1.5.0b4 so we don't have file compat issues.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     ],
     packages=find_packages(),
     install_requires=[
-        'base_kivy_app', 'ffpyplayer', 'kivy', 'nixio>=1.5.0b4', 'numpy',
+        'base_kivy_app', 'ffpyplayer', 'kivy', 'nixio==1.5.0b4', 'numpy',
         'h5py', 'kivy_garden.graph>=0.4.0', 'kivy_garden.tickmarker>=3.0.0',
         'kivy_garden.painter~=0.2.2', 'kivy_garden.collider~=0.1.1', 'tables',
         'pandas', 'xlsxwriter', 'tree-config', 'more_kivy_app'
