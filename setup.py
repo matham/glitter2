2f6cf62daa1d32bf160cb815af948fcfb7483983
Matthew Einhorn <moiein2000@gmail.com>
2020-10-08 13:27:50 -0400

    We need the point shape.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires=[
         'base_kivy_app', 'ffpyplayer', 'kivy', 'nixio>=1.5.0b4', 'numpy',
         'h5py', 'kivy_garden.graph>=0.4.0', 'kivy_garden.tickmarker>=3.0.0',
-        'kivy_garden.painter~=0.2.1', 'kivy_garden.collider~=0.1.1', 'tables',
+        'kivy_garden.painter~=0.2.2', 'kivy_garden.collider~=0.1.1', 'tables',
         'pandas', 'xlsxwriter', 'tree-config', 'more_kivy_app'
     ],
     extras_require={
