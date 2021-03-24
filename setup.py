3768b8940912d53a7ebe192d17579433ace653fe
Matthew Einhorn <moiein2000@gmail.com>
2021-03-24 17:16:55 -0400

    Simplify install.

--- setup.py
+++ setup.py
@@ -29,11 +29,11 @@ setup(
     ],
     packages=find_packages(),
     install_requires=[
-        'base_kivy_app', 'ffpyplayer', 'kivy', 'nixio==1.5.0b4',
-        'numpy', 'h5py', 'kivy_garden.graph>=0.4.0',
-        'kivy_garden.tickmarker>=3.0.0', 'kivy_garden.painter~=0.2.2',
+        'base_kivy_app~=0.1.1', 'ffpyplayer', 'kivy~=2.0.0', 'nixio==1.5.0b4',
+        'numpy', 'h5py', 'kivy_garden.graph~=0.4.0',
+        'kivy_garden.tickmarker~=3.0.0', 'kivy_garden.painter~=0.2.4',
         'kivy_garden.collider~=0.1.1', 'tables', 'pandas', 'xlsxwriter',
-        'tree-config', 'more_kivy_app'
+        'tree-config~=0.1.1'
     ],
     extras_require={
         'dev': ['pytest>=3.6', 'pytest-cov', 'flake8', 'sphinx-rtd-theme',
