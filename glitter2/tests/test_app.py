eb60c9b700744299c6fae633f2fec69576c09d73
Matthew Einhorn <matt@einhorn.dev>
2022-03-21 16:38:44 -0400

    See where file is saved.

--- glitter2/tests/test_app.py
+++ glitter2/tests/test_app.py
@@ -55,6 +55,10 @@ async def test_code_data_in_gui(
         glitter_app: Glitter2TestApp, sample_video_file):
     last_frame = None
     data_file = sample_video_file.with_suffix('.h5')
+    from os.path import isdir, expanduser
+    print(
+        sample_video_file, data_file, glitter_app.storage_controller.root_path,
+        isdir(glitter_app.storage_controller), expanduser('~'))
     assert get_contents(sample_video_file) == {sample_video_file.name}
     glitter_app.storage_controller.ui_open_file(str(sample_video_file))
 
